c5860ddc7ff1d41f1c281eeb6b33b99f3aad9817
Michael Sutherland <catsake@gmail.com>
2015-06-29 10:39:35 -0700

    Small Fixes

--- nameCSV.py
+++ nameCSV.py
@@ -6,18 +6,6 @@ Case Name | Deponent Name | Date of Deposition
 """
 
 print "\n"
-
-def line_read(file):
-	# a method for reading each line in the file and returning a tuple of case name, :, deponent and date
-	sep_list = []
-	with(open(file)) as input:
-		for line in input:
-			sep_list.append(line.partition(":"))
-			# sep_list is now a list of tuples with the following structure:
-			# (case name, ":", Name and date of deposition)
-	return sep_list
-	
-	
 	
 def date_separate(string_with_date):
 	# takes a string with a date ##/##/## OR #/#/## and returns only that date as a string
@@ -53,48 +41,43 @@ def single_char_is_int(char):
 	print char
 			
 def get_sep_list(file):	
-	sep_list = []
-	with(open(file)) as input:
-		for line in input:
-			sep_list.append(line.partition(":"))
-			# sep_list is now a list of tuples with the following structure:
-			# (case name, ":", Name and date of deposition)
-	return sep_list
+	try:
+		sep_list = []
+		with(open(file)) as input:
+			for line in input:
+				sep_list.append(line.partition(":"))
+				# sep_list is now a list of tuples with the following structure:
+				# (case name, ":", Name and date of deposition)
+		return sep_list
+	except:
+		print "Something went wrong reading file!"
 
 def format_row(file):
 	output_list = []
 	for line in get_sep_list(file):
-		(case_name, semicolon, name_depodate) = line
 		# call get_sep_list on the file, and take each line from that file (a tuple)
 		# create three variables, "case name" and "name_depodate" are the two important ones
+		(case_name, semicolon, name_depodate) = line
+		
+		# in the third item of the tuple ("names_split") we split into a 
+		# list of strings "firstname lastname ##/##/##" (which are separated by commas)
 		names_split = [x for x in name_depodate.split(",")]
-		# in the third item of each tuple ("names_split") we split into a list of strings "firstname lastname ##/##/##"
+		
 		for name in names_split:
-		# for each name in the list "names_split"
 			date = date_separate(name)
-			# get the date
-			name = name.split()
 			# split "name" into a list of its characters
+			name = name.split()
 			name_char_list = []
-			# name_char_list is the new list which we will write to
-
 			for char in name:
 				# why does this return names separated by spaces? Why not individual characters?
 				if any(letter.isdigit() for letter in char) == False:
 					name_char_list.append(char)
 					name_char_list.append(' ')
-			
-			# there are problems with this code, I'm not sure what.
-			'''
-			if name_char_list[1] == " ":
-				del name_char_list[1]
-			# remove the first char in the list (if a space)
-			'''
-			
+					
+			# remove the last character in the name, if it is a space
 			if len(name_char_list) > 1:
 				if name_char_list[len(name_char_list) - 1] == " ":
 					del name_char_list[len(name_char_list) - 1]
-			# remove last char in the list (if a space)
 			
 			name = ''.join(name_char_list)
 			
@@ -102,15 +85,17 @@ def format_row(file):
 			line_export = "\"" + case_name + "\"," + name + ", " + date + "\n"
 			output_list.append(line_export)
 	return output_list
-	print output_list
 	
 def write_csv(input_list, output_csv):
-	with open(output_csv, "w") as file:
-		file.write("CASE NAME, DEPONENT NAME, DATE\n")
-		for i in input_list:
-			file.write(i)
-	file.close()
-	print "Completed writing to file!"
+	try:
+		with open(output_csv, "w") as file:
+			file.write("CASE NAME, DEPONENT NAME, DATE\n")
+			for i in input_list:
+				file.write(i)
+		file.close()
+		print "Completed writing to file!"
+	except:
+		print "Error writing to file!"
 		
 			
 # test here		
